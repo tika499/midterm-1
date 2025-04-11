2f41e56ca9071f10b02253c090f34744d903e6ea
tika499 <t_gvalia@cu.edu.ge>
2025-04-11 15:57:12 +0400

    Initial commit

---
+++ task-03/index.js
@@ -0,0 +1,29 @@
+/**
+ * პირობა იხილეთ HTML ფაილში
+ */
+const users = [
+  {
+    id: 1,
+    name: "Alex Johnson",
+    email: "alex@example.com",
+    avatar: "https://fakeimg.pl/150x150",
+  },
+  {
+    id: 2,
+    name: "Sarah Williams",
+    email: "sarah@example.com",
+    avatar: "https://fakeimg.pl/150x150",
+  },
+  {
+    id: 3,
+    name: "Michael Brown",
+    email: "michael@example.com",
+    avatar: "https://fakeimg.pl/150x150",
+  },
+  {
+    id: 4,
+    name: "Emily Davis",
+    email: "emily@example.com",
+    avatar: "https://fakeimg.pl/150x150",
+  },
+];
---
+++ task-04/index.js
@@ -0,0 +1,66 @@
+/**
+ * პირობა იხილეთ HTML ფაილში
+ */
+
+const users = [
+  {
+    id: 1,
+    name: "Alex Johnson",
+    email: "alex@example.com",
+    avatar: "https://fakeimg.pl/150x150",
+  },
+  {
+    id: 2,
+    name: "Sarah Williams",
+    email: "sarah@example.com",
+    avatar: "https://fakeimg.pl/150x150",
+  },
+  {
+    id: 3,
+    name: "Michael Brown",
+    email: "michael@example.com",
+    avatar: "https://fakeimg.pl/150x150",
+  },
+  {
+    id: 4,
+    name: "Emily Davis",
+    email: "emily@example.com",
+    avatar: "https://fakeimg.pl/150x150",
+  },
+  {
+    id: 5,
+    name: "David Wilson",
+    email: "david@example.com",
+    avatar: "https://fakeimg.pl/150x150",
+  },
+  {
+    id: 6,
+    name: "Jessica Garcia",
+    email: "jessica@example.com",
+    avatar: "https://fakeimg.pl/150x150",
+  },
+  {
+    id: 7,
+    name: "Daniel Rodriguez",
+    email: "daniel@example.com",
+    avatar: "https://fakeimg.pl/150x150",
+  },
+  {
+    id: 8,
+    name: "Ashley Martinez",
+    email: "ashley@example.com",
+    avatar: "https://fakeimg.pl/150x150",
+  },
+  {
+    id: 9,
+    name: "Kevin Anderson",
+    email: "kevin@example.com",
+    avatar: "https://fakeimg.pl/150x150",
+  },
+  {
+    id: 10,
+    name: "Brittany Thomas",
+    email: "brittany@example.com",
+    avatar: "https://fakeimg.pl/150x150",
+  },
+];
